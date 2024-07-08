c4b9079425488d9bc19bc00b61a4b3c953fa433c
manjeetrwt <rawatmanjeet0003@gmail.com>
2024-07-08 17:08:13 +0530

    "SwitchCase Calci"

--- switch.cpp
+++ switch.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main(){
     int a,b;
-    cout<<"enter the first num\n";
+    cout<<"enter the first num \n";
     cin>>a;
     cout<<"enter the Second num\n";
     cin>>b;
