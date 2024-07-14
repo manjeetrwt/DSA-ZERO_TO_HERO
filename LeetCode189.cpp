8d607e9dbfa3e050e219e44f98fa1ce3d4d120b1
manjeetrwt <rawatmanjeet0003@gmail.com>
2024-07-14 22:59:40 +0530

    "ROTATED ARRAY"

---
+++ LeetCode189.cpp
@@ -0,0 +1,13 @@
+//ROTATED ARRAY
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector<int> temp(nums.size());
+
+        for(int i=0;i<nums.size();i++){
+            temp[(i+k) % nums.size()] = nums[i];
+        }
+        nums=temp;
+        
+    }
+};
