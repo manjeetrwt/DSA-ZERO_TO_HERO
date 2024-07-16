09c8db07904f6a6c5dcd81ab006122309f2035ea
manjeetrwt <rawatmanjeet0003@gmail.com>
2024-07-16 23:31:16 +0530

    "REMOVING ALL THE OCCURANCE OF A STRING"

---
+++ LeetCode1910.cpp
@@ -0,0 +1,11 @@
+//REMOVING ALL THE OCCURANCE OF A STRING
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+
+        while(s.length()!=0 && s.find(part) < s.length()){
+            s.erase(s.find(part),part.length());
+        }
+        return s;
+    }
+};
